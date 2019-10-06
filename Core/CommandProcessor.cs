13c25b49b213e155d8df3112bb44428f84e157f5
sappho192 <sappho192@gmail.com>
2019-10-07 00:02:47 +0900

    Now can opt-in/out users from subscribedUsers by reading their messages

--- Core/CommandProcessor.cs
+++ Core/CommandProcessor.cs
@@ -16,8 +16,8 @@ namespace ICENoticeBot.Core
             if (text.IndexOf('/') == 0)
             {// Command
                 string command = text.Substring(1);
-                answerText = $"You requested {command}";
-
+                //answerText = $"You requested {command}";
+                answerText = ExecuteCommand(messageID, userID, command);
             }
             else
             {// Message
@@ -26,6 +26,37 @@ namespace ICENoticeBot.Core
 
             answerUrl = $"https://api.telegram.org/{APIKey}/sendMessage?chat_id={userID}&reply_to_message_id={messageID}&text={answerText}";
             return answerUrl;
+        }
+
+        private string ExecuteCommand(int messageID, int userID, string command)
+        {
+            string result;
+            switch (command)
+            {
+                case "sub":
+                    if(UserManager.Instance().Add(userID))
+                    {
+                        result = @"공지사항이 새로 올라오면 알려드릴게요.";
+                    } else
+                    {
+                        result = @"이미 알림 목록에 계셔요.";
+                    }
+                    break;
+                case "unsub":
+                    if (UserManager.Instance().Remove(userID))
+                    {
+                        result = @"이제 알려드리지 않을거에요.";
+                    }
+                    else
+                    {
+                        result = @"이미 알림 목록에 없으셔요.";
+                    }
+                    break;
+                default:
+                    result = @"그런 명령은 없습니다.";
+                    break;
+            }
+            return result;
         }
     }
 }
