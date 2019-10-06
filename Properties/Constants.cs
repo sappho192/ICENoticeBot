675b5df79e0d4f71a7d0b9be561baa1ec51c79f2
sappho192 <sappho192@gmail.com>
2019-10-06 19:07:23 +0900

    Create Constants.cs

---
+++ Properties/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ICENoticeBot.Properties
+{
+    public static class Constants
+    {
+        public const string TELEGRAM_SETTINGS_PATH = @"TelegramSettings.json";
+        public const string TELEGRAM_SETTINGS_DEFAULT_PATH = @"TelegramSettings.DEFAULT.json";
+        public const string SUB_USERS_PATH = @"subscribedUsers.json";
+        public const string NOTICE_HEADERS_PATH = @"noticeHeaders.json";
+        public const string NOTICE_URL = @"http://dept.inha.ac.kr/user/indexSub.do?codyMenuSeq=6669&siteId=ice&dum=dum&boardId=5396814&page=";
+    }
+}
